4f16d03dee71d955bfd68196b5b2ab137a121eff
Ben Kasper <bckasper@gmail.com>
2021-08-08 22:46:43 -0400

    added a logical plan for how my JS code should work on the bottom of the script.js file

--- Assets/script.js
+++ Assets/script.js
@@ -12,3 +12,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+/* 
+Logical plan:
+
+1. Ask the user how many characters they want between 8-128 characters (check if there is a validation option for window prompts)
+
+2. Ask the user with a series of 4 window prompts whether they would like: 
+    a. Uppercase letters
+    b. Lowercase letters
+    c. Numeric values
+    d. Special characters
+
+3. Set up a validation to make sure at least one of each of the selected characters is chosen 
+
+4. Password is created and displayed in the textarea on the page
+
+*/
