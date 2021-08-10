a84a64470447824772836f13e3754f0fcc97cdb2
Ben Kasper <bckasper@gmail.com>
2021-08-09 21:03:20 -0400

    Added random character generator functions to get a random lowercase, uppercase, number, and special character

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Here are the generators for the various character types
+  //Function for a random upper case letter using the String.fromCharCode method
+  function randomUpperCase(){
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 65))
+  }
+  // Function for a random lower case letter using the String.fromCharCode method
+  function randomLowerCase(){
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 97))
+  }
+  // Function for a random numerical value using the String.fromCharCode method
+  function randomNumber(){
+    return String.fromCharCode(Math.floor((Math.random() * 10) + 48))
+  }
+  //Function for a random special character using the list provided at https://www.fileformat.info/info/charset/UTF-16/list.htm
+  function randomSpecialCharacter(){
+    var specialCharacter = '!"#$%^&*(){}[];:"~`<>,.?/|';
+    return specialCharacter[Math.floor(Math.random()*specialCharacter.length)]
+  }
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  passwordLength = window.prompt("How many characters do you want it?")
+  uppers = window.confirm("Do you want to include uppercase letters?")
+  lowers = window.confirm("Do you want to include lowercase letters?")
+  numbers = window.confirm("Do you want to include numeric values?")
+  characters = window.confirm("Do you want to include special characters?")
+  test = window.confirm()
+
+  generatePassword = () => {
+    // write my code to generate the password
+  
+  }
+
+  var password = generatePassword()
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
