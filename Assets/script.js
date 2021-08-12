ed5c6312d1e722b63f53d8608bd388bb9e72407b
Ben Kasper <bckasper@gmail.com>
2021-08-11 21:37:27 -0400

    Made a few updates to the README. In the JavaScript file, updated the passwordLength if statement to have all error arguments in one statement to receive an error alert. I also updated the comments on the file.

--- Assets/script.js
+++ Assets/script.js
@@ -1,51 +1,50 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Here are the generators for the various character types
-  //Function for a random upper case letter using the String.fromCharCode method; This is a method found researching online that seemed cleaner than my original plan to write out arrays of each character set and pull random values from.
+//Below are the GENERATOR FUNCTIONS for the various character types
+  //Function for a random UPPER CASE letter using the String.fromCharCode method
   function randomUpperCase(){
     return String.fromCharCode(Math.floor(Math.random() * 26 + 65))
   }
-  // Function for a random lower case letter using the String.fromCharCode method
+  // Function for a random LOWER CASE letter using the String.fromCharCode method
   function randomLowerCase(){
     return String.fromCharCode(Math.floor(Math.random() * 26 + 97))
   }
-  // Function for a random numerical value using the String.fromCharCode method
+  // Function for a random NUMERICAL VALUE using the String.fromCharCode method
   function randomNumber(){
     return String.fromCharCode(Math.floor((Math.random() * 10) + 48))
   }
-  //Function for a random special character using the list provided at https://www.fileformat.info/info/charset/UTF-16/list.htm
+  //Function for a random SPECIAL CHARACTER using the list provided at https://www.fileformat.info/info/charset/UTF-16/list.htm
   function randomSpecialCharacter(){
     var specialCharacter = '!"#$%^&*(){}[];:"~`<>,.?/|';
     return specialCharacter[Math.floor(Math.random()*specialCharacter.length)]
   }
 
 
-// Write password to the #password input
+ // Below is the function that will run a series of prompts to receive and validate the user's inputs
 function writePassword() {
 
-  // Here are the series of prompts to get the user's input
-  // Password length with an error alert if they do not enter a NUMBER 8-128 characters or it will reset if they just hit cancel
+  // Password length with an error alert if they do not enter a NUMBER 8-128 (errors if blank, <8, >128, or not a number)
+  // The user will just return to the beginning if they hit "cancel"
   passwordLength = window.prompt("What is your desired character length for the password?\n\nPlease enter a number between 8 and 128:")
-  if(!passwordLength){
-    return 
-  } else if ((passwordLength <8) || (passwordLength >128) || (isNaN(passwordLength))){
+  if((!passwordLength) || (passwordLength <8) || (passwordLength >128) || (isNaN(passwordLength))){
     return window.alert("Error: Password length must be a number between 8 to 128 characters long")
   }
 
-  // Here are the prompts for each kind of character the user wants to include
+  // Below are the prompts for each kind of character the user wants to include
   uppers = window.confirm("Would you like your password to include upper case letters (e.g., ASDF)?\n\nPlease choose \"OK\" to include, or \"Cancel\" to exclude:")
   lowers = window.confirm("Would you like your password to include lower case letters (e.g., asdf)?\n\nPlease choose \"OK\" to include, or \"Cancel\" to exclude:")
   numbers = window.confirm("Would you like your password to include numerical values (e.g., 0-9)?\n\nPlease choose \"OK\" to include, or \"Cancel\" to exclude:")
   characters = window.confirm("Would you like your password to include special characters (e.g., !@#$)?\n\nPlease choose \"OK\" to include, or \"Cancel\" to exclude:")
   
-  // Check to see that the user at least chose one kind of character
+  // Check to see that the user at least chose one kind of character; Will error and send to the start if the do not select at least one.
   if((!uppers) && (!lowers) && (!numbers) && (!characters)){
     return window.alert("Error: Password unable to generate\n\nYou must select \"OK\" for at least one kind of character.")
   }
 
+// Below is the function that will loop through each character type and add it to the generated password if the user selected to include it.
   generatePassword = () => {
-    // Code to generate the password
+    
     let string = []
     for(let i=0; i<passwordLength; i+0){
       if(uppers && i<passwordLength){
@@ -75,39 +74,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+//Event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-/* 
-Logical plan:
-
-1. Ask the user how many characters they want between 8-128 characters (check if there is a validation option for window prompts)
-
-2. Ask the user with a series of 4 window prompts whether they would like: 
-    a. Uppercase letters
-    b. Lowercase letters
-    c. Numeric values
-    d. Special characters
-
-3. Set up a validation to make sure at least one of each of the selected characters is chosen 
-
-4. Password is created and displayed in the textarea on the page
-
-*/
-
-var rando = '$fS$fS$fS$fS$fS$fS$fS$fS'
-console.log(rando.length)
-
-
-// Prior attempt with a For Loop
-// generatePassword = () => {
-//   // Code to generate the password
-//   let string = [];
-//   for(let i = 0; i < passwordLength; i++){
-//     string.push(randomUpperCase())
-//     string.push(randomLowerCase())
-//     string.push(randomNumber())
-//     string.push(randomSpecialCharacter())
-//   }
-//   return string.join("")
-// }
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Password-Generator
+This is a simple application that will take a series of user inputs and generate a random password. 
+
+## How does this application work?
+**Here is the step-by-step algorithm for this application:**
+
+1. The user opens the application using the Live URL link ()
+2. The user 
+
+
+
+
+
+## Accessing the application
+Below are the links to the live application (.io URL) and the link to the Github Repo.
+
+**Live URL:** 
+
+**Github Repo:** https://github.com/bckasper/Password-Generator 
+
+
+
+### Screenshot of application when user first opens
+
+### Screenshot of a user prompt
+
+## Screenshot of the application after it generates a password
